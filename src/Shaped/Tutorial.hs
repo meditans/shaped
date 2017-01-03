a3ccc80f6490aeb39b28edca4b98bcdd113b71af
meditans <meditans@gmail.com>
2017-01-03 12:15:06 +0100

    Renamed the example from User3 to User etc
    As we now have only this example to work with

--- src/Shaped/Tutorial.hs
+++ src/Shaped/Tutorial.hs
@@ -18,36 +18,36 @@ import qualified GHC.Generics as GHC
 
 -- I'd like the user of this library to be able to continue using his normal
 -- datatypes, e.g.:
-data User3 = User3 { userName3 :: Text, userAge3 :: Int, userDouble3 :: Double }
+data User = User { userName :: Text, userAge :: Int, userDouble :: Double }
   deriving (Show, Read, Eq, Ord, GHC.Generic)
 
-instance Generic User3
+instance Generic User
 
-exUser3 = User3 "Carlo" 26 (-0.345)
+exUser = User "Carlo" 26 (-0.345)
 
-exValidation :: UserShape3 (Validation (Either Text))
-exValidation = UserShape3
+exValidation :: UserShape (Validation (Either Text))
+exValidation = UserShape
   (Validation . Comp $ \t -> if t == "Carlo" then Right t else Left "Incorrect name")
   (Validation . Comp $ \a -> if a < 100      then Right a else Left "No cyborgs here")
   (Validation . Comp $ \d -> if d > 0        then Right d else Left "Positives values only")
 
 -- And be able to call the generic validation function in this way:
-exError :: UserShape3 (Either Text)
-exError = validateRecord exUser3 exValidation
+exError :: UserShape (Either Text)
+exError = validateRecord exUser exValidation
 
 -- There is still a bit of boilerplate, to be automatically generated:
 
-data UserShape3 f = UserShape3 (f Text) (f Int) (f Double)
+data UserShape f = UserShape (f Text) (f Int) (f Double)
   deriving (GHC.Generic)
-instance Generic (UserShape3 f)
+instance Generic (UserShape f)
 
-instance Shaped User3 UserShape3 where
-  toShape   (User3 n a d) = UserShape3 (Identity n) (Identity a) (Identity d)
-  fromShape (UserShape3 (Identity n) (Identity a) (Identity d)) = User3 n a d
+instance Shaped User UserShape where
+  toShape   (User n a d) = UserShape (Identity n) (Identity a) (Identity d)
+  fromShape (UserShape (Identity n) (Identity a) (Identity d)) = User n a d
 
-instance (Show1 f) => Show (UserShape3 f) where
-  showsPrec d (UserShape3 x y z) = showParen (d > 10) $
-      showString "UserShape3"
+instance (Show1 f) => Show (UserShape f) where
+  showsPrec d (UserShape x y z) = showParen (d > 10) $
+      showString "UserShape"
     . showChar ' ' . showsPrec1 11 x
     . showChar ' ' . showsPrec1 11 y
     . showChar ' ' . showsPrec1 11 z
