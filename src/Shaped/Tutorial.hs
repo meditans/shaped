4d421043ec4649ffb4059446ec4d01d96cd31e6b
meditans <meditans@gmail.com>
2017-01-03 13:16:08 +0100

    Concrete version for transf, which summarize the error
    transf :: UserShape (Either Text) -> Either (UserShape (Const (Maybe Text))) User
    
    I used Const because it's in base and doesn't force me to import generics-sop in
    the use sites.

--- src/Shaped/Tutorial.hs
+++ src/Shaped/Tutorial.hs
@@ -1,11 +1,17 @@
 {-# LANGUAGE DeriveGeneric, MultiParamTypeClasses, OverloadedStrings #-}
 
+{-# LANGUAGE DataKinds #-}
+
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# OPTIONS_GHC -fdefer-typed-holes #-}
+
 module Shaped.Tutorial where
 
 import Data.Text
 import Shaped
 
 import Control.Monad.Identity
+import Data.Functor.Const
 import Data.Functor.Classes
 import Data.Functor.Compose
 
@@ -25,6 +31,8 @@ instance Generic User
 
 exUser = User "Carlo" 26 (-0.345)
 
+validUser = User "Carlo" 26 0.434
+
 exValidation :: UserShape (Validation (Either Text))
 exValidation = UserShape
   (Validation . Comp $ \t -> if t == "Carlo" then Right t else Left "Incorrect name")
@@ -34,6 +42,41 @@ exValidation = UserShape
 -- And be able to call the generic validation function in this way:
 exError :: UserShape (Either Text)
 exError = validateRecord exUser exValidation
+
+exOk :: UserShape (Either Text)
+exOk = validateRecord validUser exValidation
+
+transf :: UserShape (Either Text) -> Either (UserShape (Const (Maybe Text))) User
+transf us =
+  let sequenced = hsequence struct
+  in case sequenced of
+       Right u -> Right (to u)
+       Left  _ -> Left  (to . toSOPI . hmap toMaybe $ struct)
+  where
+    struct :: SOP (Either Text) '[ '[ Text, Int, Double ] ]
+    struct = fromSOPI . from $ us
+
+-- This has to be a natural transformation, per hmap type.
+toMaybe :: Either Text t -> Const (Maybe Text) t
+toMaybe x = case x of
+  Right a -> Const Nothing
+  Left  b -> Const (Just b)
+
+-- Now we want to get back from a `UserShape (Either Text)` to a simpler `Either
+-- (UserShape (Maybe Text)) User`, i.e. extracting the error to present the
+-- eventual error in each field.
+
+-- We have:
+
+-- hsequence' :: (SListIN h xs, Applicative f) => h (f :.: g) xs -> f (h g xs)
+
+-- like in:
+
+-- hsequence' :: (SListI2 xss, Applicative f) => SOP (f :.: g) xss -> f (SOP g xss)
+
+-- Or some of the restricted versions:
+
+-- hsequence :: (SListIN h xs, SListIN (Prod h) xs, HSequence h) => Applicative f => h f xs -> f (h I xs)
 
 -- There is still a bit of boilerplate, to be automatically generated:
 
@@ -51,3 +94,4 @@ instance (Show1 f) => Show (UserShape f) where
     . showChar ' ' . showsPrec1 11 x
     . showChar ' ' . showsPrec1 11 y
     . showChar ' ' . showsPrec1 11 z
+
